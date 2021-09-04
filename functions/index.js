2bcf6ef8b8c334f7fb654f621118f36835984d74
Pat <patmifsud@me.com>
2021-09-04 16:45:53 +1000

    Triggers an event when front end updates the database

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,17 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
-exports.buildPageRequest = functions.https.onCall((data, context) => {
-   // check if we have enough deploy tokens in service/deploys
-   return `hello`;
-}); 
+admin.initializeApp();
+
+
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document('/{collection}/{id}')
+  .onCreate((snap, context) => {
+     
+      console.log("new entry to DB");
+
+    console.log(snap.data());
+
+    return null;
+});
