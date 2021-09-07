8c0f4f704cc309ac7eb7f0407eb18043dc1eee9d
Pat <patmifsud@me.com>
2021-09-07 17:54:39 +1000

    Token checking works now, added api key to environment variable

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { service } = require("firebase-functions/v1/analytics");
 
 // firebase emulators:start
 // http://localhost:5000
@@ -16,106 +15,113 @@ const config = {
 exports.siteAdded = functions.firestore
   .document("/sites/{id}")
   .onCreate((snap, context) => {
-    const limiterPath = admin.firestore().collection("backend").doc("limiter");
-    let success = true;
-
-   // Do we have query tokens? 
-   limiterPath.get()
-
-      .then((doc) => {
-         if (doc.exists) {
-            console.log("Connected to backend:", doc.data());
-            return(doc.data());
-         } else throw "Unable to connect to backend DB";
-      })
-
-      .then(async (limiterStatus) => {
-         console.log("Limiter status");
-         if (limiterStatus.date != generateDateStr()) {
-            await limiterReset();
-         }
-         if (limiterStatus.tokens < 1) {
-            throw "Not enough request tokens remaining for today";
-         }
-         return(limiterStatus);
-      })
-      
-      .then((limiterStatus) => {
-         removeToken(limiterStatus.tokens);
-      })
-      
-      .then(() => {
-         console.log("api stuff here")
-      })
-      
-      .catch((error) => {
-         console.error("Unable to create new site: ", error);
-         success = false;
-      });
-
-
-    //-------------------------------------------
-    // Token Limiter functions
-
-
-
-    function removeToken(tokenNum) {
-      const date = generateDateStr();
-      const newTokenValue = tokenNum - 1;
-
-      limiterPath
-        .set({
-          date: date,
-          tokens: newTokenValue
-        })
-        .then(() => {
-          console.log(
-            `One token has been removed, ${newTokenValue} remaining today`
-          );
-          return Promise.resolve();
-        })
-        .catch(() => {
-          console.error("Unable to remove a token");
-          success = false;
-        });
-    }
-
-
-    function generateDateStr() {
-      const now = new Date();
-      return `${now.getUTCFullYear()}-${now.getMonth()}-${now.getDay()}`;
-    }
-
-
-
-  function limiterReset() {
+    return new Promise((resolve, reject) => {
+      let limiterStatus = {};
+      let prompt = snap.data();
+      let key = functions.config().openai.key;
       const date = generateDateStr();
 
-      console.log(
-        `reseting limiter to ${config.dailyTokens} tokens for date ${date}`
-      );
+      const limiterPath = admin
+        .firestore()
+        .collection("backend")
+        .doc("limiter");
 
-      try {
-         limiterPath
-         .set({
-            date: date,
-            tokens: config.dailyTokens,
-         }).then(() => {
-            console.log(`limiter has been updated`);
-         })
+      // Get limiter data from db
+      function getLimiter() {
+        limiterPath
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              console.log("Connected to backend:", doc.data());
+              limiterStatus = {
+                date: doc.data().date,
+                tokens: doc.data().tokens,
+              };
+              checkLimiter();
+            } else throw error("No data found");
+          })
+          .catch((error) => {
+            console.error(error);
+            reject("Unable to get limiter data");
+          });
       }
-         catch(error){
-          console.error("Unable to reset limiter", error);
-          success = false;
-      };
 
-      return Promise.resolve();
-    }
+      // Check if limiter date is from today, and tokens are > 1
+      function checkLimiter() {
+        console.log("Checking the limiter");
+        if (limiterStatus.date != date) {
+          limiterReset();
+        } else if (limiterStatus.tokens < 1) {
+          throw "Not enough request tokens remaining for today";
+        } else {
+          removeToken();
+        }
+      }
 
-    //-------------------------------------------
-    // GPT functions
+      // If limiter date isn't today, set date to today and set tokens to full
+      function limiterReset() {
+        console.log(
+          `reseting limiter to ${config.dailyTokens} tokens for date ${date}`
+        );
+        try {
+          limiterPath
+            .set({
+              date: date,
+              tokens: config.dailyTokens,
+            })
+            .then(() => {
+              console.log(`limiter has been updated`);
+              removeToken();
+            });
+        } catch (error) {
+          reject("Unable to reset limiter");
+        }
+      }
 
- 
+      // Make date string
+      function generateDateStr() {
+        const now = new Date();
+        return `${now.getUTCFullYear()}-${now.getMonth()}-${now.getDay()}`;
+      }
 
-    return Promise.resolve(); 
+      // Make date string
+      async function getKey() {
+         try {
+            return functions.config().openai.key;
+          } catch (error) {
+            console.log("couldn't get the openai key. ")
+            key = 'diio'
+          }
+      }
+
+      // Remove 1 token from the database
+      function removeToken() {
+        const newTokenValue = parseInt(limiterStatus.tokens) - 1;
+
+        limiterPath
+          .set({
+            date: date,
+            tokens: newTokenValue,
+          })
+          .then(() => {
+            console.log(
+              `One token has been removed, ${newTokenValue} remaining today`
+            );
+            doNextThing();
+          })
+          .catch(() => {
+            console.error("Unable to remove a token");
+            reject("Unable to remove a token");
+          });
+      }
+
+      // Remove 1 token from the database
+      function doNextThing() {
+        console.log(prompt);
+        console.log("key:", key);
+        resolve("completed");
+      }
+
+      getLimiter();
     });
+  });
